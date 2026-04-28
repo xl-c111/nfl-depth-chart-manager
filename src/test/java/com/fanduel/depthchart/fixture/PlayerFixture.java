0468b4af7cefc13d71b2201c6538ce008aa1598e
Xiaoling Cui <10430@holbertonstudents.com>
2026-04-29 00:57:32 +1000

    Add tests for add-player depth behavior

---
+++ src/test/java/com/fanduel/depthchart/fixture/PlayerFixture.java
@@ -0,0 +1,12 @@
+package com.fanduel.depthchart.fixture;
+
+import com.fanduel.depthchart.domain.Player;
+
+public final class PlayerFixture {
+    public static final Player TOM_BRADY = new Player(12, "Tom Brady");
+    public static final Player BLAINE_GABBERT = new Player(11, "Blaine Gabbert");
+    public static final Player KYLE_TRASK = new Player(2, "Kyle Trask");
+
+    private PlayerFixture() {
+    }
+}
